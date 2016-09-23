f5250564f84a6be66ba3f50160ff0a89a94c2358
Francesco Novy <francesco@cropster.com>
2016-09-23 16:44:44 +0200

    Basic bar-chart component setup

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
+import d3 from 'd3';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+
+  data: [
+    {
+      name: 'John',
+      value: 31
+    },
+    {
+      name: 'Anne',
+      value: 33
+    },
+    {
+      name: 'Robert',
+      value: 28
+    }
+  ],
+
+  addSVG: function() {
+    // TODO: Generate the base SVG object
+  },
+
+  drawData: function() {
+    // TODO: Draw the data
+  },
+
+  createChart: function() {
+    // Clear the element, if there is something inside
+    var chartEl = this.$().get(0);
+    chartEl.innerHTML = '';
+
+    // Actually create the SVG element
+    this.addSVG();
+
+    // Draw the data
+    this.drawData();
+  },
+
+  // -----------------------------------------------------------------------
+  // LIFECYCLE HOOKS
+  // These are special functions that are called by ember at different stages
+  // of the component's lifecycle.
+  // -----------------------------------------------------------------------
+
+  didInsertElement: function() {
+    this.createChart();
+  }
+
 });
